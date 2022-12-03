28a54e3f26aacb152dc554d424beae436c419b46
salehsyam <salehsyam97@gmail.com>
2022-12-03 12:32:12 +0200

    add-pug

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const app = express();
 const path = require('path');
-const adminRoute = require('./routes/admin');
-const shopRoute = require('./routes/shop');
 
-//requires a middleware function
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// Route
-app.use('/admin', adminRoute);
-app.use(shopRoute);
+const app = express();
 
-// Not Found routes
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000!');
-});
+app.listen(3000);
